a937e7fde010cc8804f8659f82fcc93df7b6ed2b
Unay Santisteban <usantisteban@othercode.io>
2024-01-26 16:56:31 +1000

    Feature/Add page number support. (#20)

--- src/Page.php
+++ src/Page.php
@@ -17,6 +17,9 @@ final class Page implements ValueObject
 {
     use IsValueObject;
 
+    const DEFAULT_LIMIT = 25;
+    const DEFAULT_OFFSET = 0;
+
     /**
      * Page constructor.
      *
@@ -24,15 +27,20 @@ final class Page implements ValueObject
      * @param  int  $offset
      */
     public function __construct(
-        private readonly int $limit = 25,
-        private readonly int $offset = 0,
+        private readonly int $limit = self::DEFAULT_LIMIT,
+        private readonly int $offset = self::DEFAULT_OFFSET,
     ) {
         $this->check();
     }
 
-    public static function create(int $limit = 25, int $offset = 0): self
+    public static function create(int $limit = self::DEFAULT_LIMIT, int $offset = self::DEFAULT_OFFSET): self
     {
         return new self($limit, $offset);
+    }
+
+    public static function number(int $number, int $size = self::DEFAULT_LIMIT): self
+    {
+        return self::create($size, ($size * $number) - $size);
     }
 
     protected function invariantLimitValueMustBePositive(): bool
